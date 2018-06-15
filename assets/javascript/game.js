967791954c5f31272978e3f7f0d159242d2ec6d1
Reena <reenam2017@gmail.com>
2018-06-14 23:14:03 -0700

    06-14-2018

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,36 +15,33 @@ $(document).ready(function(){
   function gen(){
       randomNumber= Math.floor((Math.random() * 100) + 1);
       red = Math.floor((Math.random() * 10) + 1);
-      green = Math.floor((Math.random() * 10) + 1);
-      blue = Math.floor((Math.random() * 10) + 1);
-      yellow = Math.floor((Math.random() * 10) + 1);
+      green = Math.floor((Math.random() * 12) + 1);
+      blue = Math.floor((Math.random() * 10) + 2);
+      yellow = Math.floor((Math.random() * 12) + 2);
   };
 
   ///Assigning the random generated number to html..................
   gen();
-  $("#random-number").html(randomNumber);
-  $("#total-score").html(guessTotal);
+  $("#random-number").text(randomNumber);
+  $("#total-score").text(guessTotal);
   $("#loss1").text(losses);
   $("#win1").text(wins);
 
-  $("#button1").click(function(){
+  $("#button1").on("click", function(){
       work(red);
   });
 
-  $("#button2").click(function(){
+  $("#button2").on("click", function(){
       work(green);
   });
 
-  $("#button3").click(function(){
+  $("#button3").on("click", function(){
       work(blue);
   });
 
-  $("#button4").click(function(){
+  $("#button4").on("click", function(){
       work(yellow);
   });
-
-  ///Displaying the value to Total Score-----------
-
 
   ////Reset function is called when the player wins or losses a game.......
   function reset() {
@@ -54,6 +51,9 @@ $(document).ready(function(){
       $("#total-score").html(guessTotal);
   };
 
+///Creating a work function where the addition of all 4 crystal values are
+//done and sum total is assign to guessTotal. Then the comparision of
+//computer random number and the guessTotal number is done.
   function work(crystalNum) {
       guessTotal += crystalNum;
       $("#total-score").empty();
@@ -67,7 +67,6 @@ $(document).ready(function(){
           $("#win1").html(wins);
           reset();
       }
-
   };
 
 });
--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+
 ////Initializing the values............
   var guessTotal = 0;
   var wins = 0;
@@ -8,57 +8,73 @@ $(document).ready(function(){
   var green = 0;
   var blue = 0;
   var yellow = 0;
-  var crystalNum;
+  var crystalNum = 0;
 
-////Creating function assign() to assign the numbers to html.....................
-  function assign() {
-      $("#random-number").text(randomNumber);
-      $("#total-score").text(guessTotal);
-      $("#loss1").text(losses);
-      $("#win1").text(wins);
-
-      $("#button1").click(function(){
-          work(red);
-      });
-
-      $("#button2").click(function(){
-          work(green);
-      });
-
-      $("#button3").click(function(){
-          work(blue);
-      });
-
-      $("#button4").click(function(){
-          work(yellow);
-      });
-  };
+  $(document).ready(function(){
 
 ////Creating a reset function which will be called when the user wins or losses a game...............
     function reset() {
         guessTotal = 0;
-        $("#random-number").html(randomNumber);
-        $("#total-score").html(guessTotal);
+        $("#random-number").text(randomNumber);
+        $("#total-score").text(guessTotal);
     };
 
 ////Creating function work where the user guess number is added...............
     function work(crystalNum) {
+      console.log("hello");
         guessTotal += crystalNum;
+        console.log(typeof crystalNum);     ///console output undefined
+        console.log(typeof guessTotal);    ///console output NaN
         $("#total-score").empty();
         $("#total-score").append(guessTotal);
         if(randomNumber < guessTotal) {
             losses++;
-            $("#loss1").html(losses);
+            $("#loss1").text(losses);
             reset();
         } else if(randomNumber === guessTotal) {
             wins++;
-             $("#win1").html(wins);
+             $("#win1").text(wins);
             reset();
         }
     };
 
+////Creating function assign() to assign the numbers to html.....................
+    function assign() {
+        $("#random-number").text(randomNumber);
+        console.log("hello world");
+          //console.log( typeof randomNumber);
+          $("#total-score").text(guessTotal);
+          //console.log( typeof guessTotal);
+          $("#loss1").text(losses);
+          $("#win1").text(wins);
+
+          $("#button1").on("click", function(){
+              work(red);
+              console.log(red);
+              //console.log(typeof red);
+          });
+
+          $("#button2").click(function(){
+              work(green);
+              console.log(green);
+          });
+
+          $("#button3").click(function(){
+              work(blue);
+              console.log(blue);
+          });
+
+          $("#button4").click(function(){
+              work(yellow);
+              console.log(yellow);
+          });
+      };
+
 /////////////......................FOR EASY LEVEL................................................................
-  $("#easy").click(function(){
+  $("#easy").on("click", function(){
+      $("#easy").fadeOut(1000);
+      $("#medium").fadeIn(1000);
+      $("#hard").fadeIn(1000);
       gen1();
       assign();
       work();
@@ -77,6 +93,9 @@ $(document).ready(function(){
 /////////////......................FOR MEDIUM LEVEL................................................................
 
     $("#medium").click(function(){
+      $("#medium").fadeOut(1000);
+      $("#easy").fadeIn(1000);
+      $("#hard").fadeIn(1000);
         gen2();
         assign();
         work();
@@ -95,6 +114,9 @@ $(document).ready(function(){
 /////////////......................FOR HARD LEVEL................................................................
 
     $("#hard").click(function(){
+      $("#hard").fadeOut(1000);
+      $("#medium").fadeIn(1000);
+      $("#easy").fadeIn(1000);
         gen3();
         assign();
         work();
@@ -109,24 +131,5 @@ $(document).ready(function(){
            blue = Math.floor((Math.random() * 100) + 1);
            yellow = Math.floor((Math.random() * 10) + 1);
        };
-
-/*      if $("#hard").click() {
-         gen3();
-         assign();
-         function work(crystalNum) {
-             guessTotal += crystalNum;
-             $("#total-score").empty();
-             $("#total-score").append(guessTotal);
-             if(randomNumber < guessTotal) {
-                 losses++;
-                 $("#loss1").html(losses);
-                     reset3();
-             } else if (randomNumber === guessTotal) {
-                 wins++;
-                 $("#win1").html(wins);
-                 reset3();
-             }
-         };
-       };*/
 
 });           //////document.ready() end bracket
